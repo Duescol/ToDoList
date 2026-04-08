53f570b344992f5049f7cd4f2c9b0759f2a34c0d
Eduhj <eduardo.jonerhahn@gmail.com>
2026-04-07 21:40:07 -0300

    Lista de tarefas

---
+++ app.js
@@ -0,0 +1,34 @@
+
+const D_tarefas = document.getElementById("tarefas")
+const B_excluir = document.getElementById("excluir")
+const B_feito = document.getElementById("feito")
+const I_texto = document.getElementById("tarefa")
+const B_add = document.getElementById("add")
+
+function CriaTarefa() {
+    const item = document.createElement("li")
+    const Bdon = document.createElement("button")
+    const Itext = document.createElement("input")
+    const Bdel = document.createElement("button")
+
+    Bdon.textContent = "✔️"
+    Bdel.textContent = "❌"
+    Itext.type = "text"
+    Itext.placeholder = "Nova tarefa..."
+
+    Bdon.classList.add("btn-icon")
+    Bdel.classList.add("btn-icon")
+    Itext.classList.add("tarefa-input")
+
+    Bdel.addEventListener("click", function () { item.remove() })
+    Bdon.addEventListener("click", function () { Itext.classList.toggle("btn-icon-done") })
+
+
+    item.appendChild(Bdon)
+    item.appendChild(Itext)
+    item.appendChild(Bdel)
+    D_tarefas.appendChild(item)
+
+}
+
+B_add.addEventListener("click", CriaTarefa)
